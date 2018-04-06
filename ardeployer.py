dad26a0ba778074565006a3a6f8b36f317b7f501
peterzhang <zhh520@gmail.com>
2018-04-06 21:02:30 +0200

    First py

---
+++ ardeployer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Jenkins Artifact Deployer """
+
+import sys
+
+if __name__ == "__main__":
+    if not sys.version_info >= (3, 6):
+        sys.exit(1)
+    print "It works"
